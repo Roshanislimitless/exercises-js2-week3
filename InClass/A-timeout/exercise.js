a7a6389dded92b14f342fd52e7de028da3cca457
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-05-07 23:24:39 +0200

    used setInterval and setTimeout to change color of background

--- InClass/A-timeout/exercise.js
+++ InClass/A-timeout/exercise.js
@@ -9,3 +9,50 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+// setTimeout(changeColor, 3000);
+
+// function changeColor(){
+//     document.body.style.background = "red";
+// }
+
+
+var colors =['green', 'white', 'blue', 'yellow', 'red'];
+index = 0;
+setInterval(changeColor, 500);
+
+function changeColor(){
+    index += 1;
+    if( index === colors.length){
+        index = 0;
+    }
+    
+   document.body.style.background = colors[index];
+}
+
+
+
+
+
+
+//...............................using Math.random and Math.floor..........................
+// function colorChange(){
+//     var colors = ['green', 'white', 'blue'];
+//     var length = colors.length;
+//     var randomIndex = Math.floor(length * Math.random());
+//     document.body.style.background = colors[randomIndex];
+// }
+
+// setInterval(colorChange, 500);
+
+
+
+
+
+
+
+
+
+
+
+
