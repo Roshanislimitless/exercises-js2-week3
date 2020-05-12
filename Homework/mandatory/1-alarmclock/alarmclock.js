1538b0b6331776213ee4ee8c03ebc2aa1c9b6d47
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-05-12 04:05:28 +0200

    alarm clock final

--- Homework/mandatory/1-alarmclock/alarmclock.js
+++ Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,21 +1,68 @@
+//................BULLSHIT done by me.........................................................
+// function setAlarm() {
+//   var Initial = document.querySelector('#alarmSet').value;
+//   setInterval(function Alarm() {
+//     if (Initial > 0) {
+//       Initial--
+//     } else if (Initial === 0) {
+//       playAlarm();
+//        document.body.style.background = "red";
+//     }
+
+
+//     document.querySelector('#timeRemaining').innerHTML = "Time Remaining: 00:" + Initial;
+
+
+
+//   }, 1000)
+// }
+//How jay did it..........practice..........................................................
+var alarmSeconds;
+var alarmInterval;
+var milliSecPerSec = 1000;
 
 function setAlarm() {
-  var Initial = document.querySelector('#alarmSet').value;
-  setInterval(function Alarm() {
-    if (Initial > 0) {
-      Initial--
-    } else if (Initial === 0) {
-      playAlarm();
-       document.body.style.background = "red";
-    }
+  var alarmInput = document.querySelector("#alarmSet");
+  alarmSeconds = alarmInput.value;
 
-
-    document.querySelector('#timeRemaining').innerHTML = "Time Remaining: 00:" + Initial;
-
-
-
-  }, 1000)
+  alarmInterval = setInterval(subtractAlarmSecond, milliSecPerSec);
 }
+
+function subtractAlarmSecond(){
+  if(alarmSeconds > 0){
+  alarmSeconds--;
+  }
+  displayTimeRemaining();
+  if(alarmSeconds === 0){
+    clearInterval(alarmInterval);
+    playAlarm()
+  }
+
+}
+
+function displayTimeRemaining(){
+  var timeRemainingH1 = document.querySelector("#timeRemaining");
+  var minutes = Math.floor(alarmSeconds / 60);
+  var seconds = alarmSeconds - (minutes*60);
+  var minutesString = '';
+
+  if( minutes < 10 ){
+    minutesString = '0' + minutes;
+  }else {minutesString = minutes;}
+  
+
+  var secondString = '';  
+  if ( seconds < 10){
+    secondString = '0'  + seconds;
+  }else { secondString = seconds;}
+
+  timeRemainingH1.innerText = "Time remaining :" + minutesString + ":" + secondString;
+  console.log(secondString);
+  
+}
+
+
+
 
 
 
